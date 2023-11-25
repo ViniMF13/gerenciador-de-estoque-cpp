7050eb895c340baacd1a5eb037a1273302c0a57c
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-11-25 18:14:05 -0300

    Class Item: tratamento de exceções

--- Item.cpp
+++ Item.cpp
@@ -9,32 +9,41 @@
 
 // Construtor padrão
 Item::Item(const std::string& nome, double valor) {
-    this->nome = nome;
-    this->valor = valor;
-    this->quantidade = 0;
-    this->id = Item::gerarId();
-    
-// Tratamento de excessões
+    try {
+        this->nome = nome;
+        this->valor = valor;
+        this->quantidade = 0;
+        this->id = Item::gerarId();
+    } catch (const std::exception& e) {
+        throw std::invalid_argument("Erro ao construir o Item: " + std::string(e.what()));
+    }
 }
 
 Item::Item(const std::string& nome, double valor, int quantidade) {
-    this->nome = nome;
-    this->valor = valor;
-    this->quantidade = quantidade;
-
-// Tratamento de excessões
+    try {
+        this->nome = nome;
+        this->valor = valor;
+        this->quantidade = quantidade;
+    } catch (const std::exception& e) {
+        throw std::invalid_argument("Erro ao construir o Item: " + std::string(e.what()));
+    }
 }
 
 // Construtor para desserialização do Item 
 Item::Item(const std::string& jsonString) {
-    nlohmann::json j = nlohmann::json::parse(jsonString);
+  try {
+      nlohmann::json j = nlohmann::json::parse(jsonString);
 
-    int idItem = j["id"].get<int>();
-    // Obtém os valores do JSON
-    this->id = idItem;
-    this->nome = j["nome"];
-    this->valor = j["valor"];
-    this->quantidade = j["quantidade"];
+      // Usando `at` para garantir que a chave exista
+      this->id = j.at("id").get<int>();
+      this->nome = j.at("nome").get<std::string>();
+      this->valor = j.at("valor").get<double>();
+      this->quantidade = j.at("quantidade").get<int>();
+  } catch (const nlohmann::json::exception& e) {
+      throw std::invalid_argument("Erro ao desserializar o Item: " + std::string(e.what()));
+  } catch (const std::exception& e) {
+      throw std::invalid_argument("Erro ao desserializar o Item: " + std::string(e.what()));
+  }
 }
 
 std::string Item::toJson() const {
@@ -81,49 +90,63 @@ void Item::setQuantidade(int quantidade){
 }
 
 void Item::cadastrarItem(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja cadastrar:");
-    
-    // Verifica se já existe um item com o mesmo nome no inventário antes de adicionar
-    if(inventario.itemExiste(nome)){
-      // Se o Item existe------- +++++++++v
-      Interface::exibirMensagem("Erro: O Item ja esta cadastrado no sistema");
-    } else {
-      // Se o Item não existe, cria o item e adiciona ao inventário ------- +++++++++v
-      double valor = Interface::lerValor<double>("Digite o valor do item:");
+    try {
+        std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja cadastrar:");
+        
+        // Verifica se já existe um item com o mesmo nome no inventário antes de adicionar
+        if(inventario.itemExiste(nome)){
+            // Se o Item existe
+            Interface::exibirMensagem("Erro: O Item já está cadastrado no sistema");
+        } else {
+            double valor = Interface::lerValor<double>("Digite o valor do item:");
 
-      Item newItem = Item(nome, valor);
-      inventario.obterEstoque().insert(std::make_pair(newItem.getId(), newItem));
-      Interface::exibirMensagem("O item foi cadastrado no inventario!");
+            Item newItem = Item(nome, valor);
+            inventario.obterEstoque().insert(std::make_pair(newItem.getId(), newItem));
+            Interface::exibirMensagem("O item foi cadastrado no inventário!");
+        }
+    } catch (const std::exception& e) {
+        // Tratar exceções relacionadas à entrada do usuário
+        Interface::exibirMensagem("Erro ao cadastrar o item: " + std::string(e.what()));
     }
 }
 
 void Item::removerItem(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja remover");
+    try {
+        std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja remover");
 
-    // Verifica se já existe o item no inventário
-    if (inventario.itemExiste(nome)) {
-      // Se o Item existe, o remove do inventário ------- +++++++++v
-      Item& item = inventario.getItem(nome);
-      auto it = inventario.obterEstoque().find(item.getId());
-      inventario.obterEstoque().erase(it);
-      
-      Interface::exibirMensagem("O item foi Removido do inventario!");
-    } else {
-      Interface::exibirMensagem("Erro: O item não está cadastrado no inventário.");
+        // Verifica se já existe o item no inventário
+        if (inventario.itemExiste(nome)) {
+            // Se o Item existe, o remove do inventário
+            Item& item = inventario.getItem(nome);
+            auto it = inventario.obterEstoque().find(item.getId());
+            inventario.obterEstoque().erase(it);
+            
+            Interface::exibirMensagem("O item foi Removido do inventário!");
+        } else {
+            Interface::exibirMensagem("Erro: O item não está cadastrado no inventário.");
+        }
+    } catch (const std::exception& e) {
+        // Tratar exceções relacionadas à entrada do usuário
+        Interface::exibirMensagem("Erro ao remover o item: " + std::string(e.what()));
     }
 }
 
 void Item::atualizarValor(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item");
+    try {
+        std::string nome = Interface::lerValor<std::string>("Digite o nome do item");
 
-    // Verifica se já existe o item no inventário
-    if (inventario.itemExiste(nome)) {
-      // Se o Item existe,  ------- +++++++++v
-       double valor = Interface::lerValor<int>("Digite o novo valor do item:");
-       inventario.getItem(nome).setValor(valor);
-       Interface::exibirMensagem("O valor do item foi autualizado");
-
-    } else {
-      Interface::exibirMensagem("O item não está cadastrado no inventário");
+        // Verifica se já existe o item no inventário
+        if (inventario.itemExiste(nome)) {
+            // Se o Item existe
+            double novoValor = Interface::lerValor<double>("Digite o novo valor do item:");
+            inventario.getItem(nome).setValor(novoValor);
+            Interface::exibirMensagem("O valor do item foi atualizado");
+        } else {
+            // Se o Item não existe
+            Interface::exibirMensagem("O item não está cadastrado no inventário");
+        }
+    } catch (const std::exception& e) {
+        // Tratar exceções relacionadas à entrada do usuário
+        Interface::exibirMensagem("Erro ao atualizar o valor do item: " + std::string(e.what()));
     }
 }
