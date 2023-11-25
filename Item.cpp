e6c1bb7e283e98815876b65b09a5c9441f3a414a
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-11-25 17:21:37 -0300

    Class Item: add ID ao item

--- Item.cpp
+++ Item.cpp
@@ -1,16 +1,19 @@
 #include "Item.hpp"
 #include "Inventario.hpp"
 #include "Interface.hpp"
-#include "Include/json.hpp"
+
+#include "Include/JSON/json.hpp"
 
 #include <iostream>
+#include <string>
 
-// Implementação dos construtores do Item
+// Construtor padrão
 Item::Item(const std::string& nome, double valor) {
     this->nome = nome;
     this->valor = valor;
     this->quantidade = 0;
-
+    this->id = Item::gerarId();
+    
 // Tratamento de excessões
 }
 
@@ -22,60 +25,41 @@ Item::Item(const std::string& nome, double valor, int quantidade) {
 // Tratamento de excessões
 }
 
+// Construtor para desserialização do Item 
 Item::Item(const std::string& jsonString) {
     nlohmann::json j = nlohmann::json::parse(jsonString);
 
+    int idItem = j["id"].get<int>();
     // Obtém os valores do JSON
+    this->id = idItem;
     this->nome = j["nome"];
     this->valor = j["valor"];
     this->quantidade = j["quantidade"];
 }
 
-void Item::cadastrarItem(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja cadastrar:");
-    double valor = Interface::lerValor<double>("Digite o valor do item:");
-    
-    // Verifica se já existe um item com o mesmo nome no inventário antes de adicionar
-    if(inventario.itemExiste(nome)){
-      // Se o Item existe------- +++++++++v
-      Interface::exibirMensagem("Erro: O Item ja esta cadastrado no sistema");
-    } else {
-      // Se o Item não existe, cria o item e adiciona ao inventário ------- +++++++++v
-      Item newItem = Item(nome, valor);
-      inventario.obterEstoque().insert(std::make_pair(newItem.getNome(), newItem));
-      Interface::exibirMensagem("O item foi cadastrado no inventario!");
-    }
+std::string Item::toJson() const {
+    nlohmann::json j;
+
+    j["id"] = id;
+    j["nome"] = nome;
+    j["valor"] = valor;
+    j["quantidade"] = quantidade;
+
+    return j.dump(); // Converte o JSON para uma string
 }
 
-void Item::removerItem(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja remover");
-
-    // Verifica se já existe o item no inventário
-    if (inventario.itemExiste(nome)) {
-      // Se o Item existe, o remove do inventário ------- +++++++++v
-      inventario.obterEstoque().erase(inventario.obterEstoque().find(nome));
-      Interface::exibirMensagem("O item foi Removido do inventario!");
-    } else {
-      Interface::exibirMensagem("Erro: O item não está cadastrado no inventário.");
-    }
-}
-
-void Item::atualizarValor(Inventario& inventario){
-    std::string nome = Interface::lerValor<std::string>("Digite o nome do item");
-
-    // Verifica se já existe o item no inventário
-    if (inventario.itemExiste(nome)) {
-      // Se o Item existe,  ------- +++++++++v
-       double valor = Interface::lerValor<int>("Digite o novo valor do item:");
-       inventario.getItem(nome).setValor(valor);
-       Interface::exibirMensagem("O valor do item foi autualizado");
-
-    } else {
-      Interface::exibirMensagem("O item não está cadastrado no inventário");
-    }
+int Item::gerarId() {
+    // Inicializa a semente do gerador de números aleatórios com o tempo atual
+    std::srand(std::time(0));
+    // Gera um número aleatório entre 1 e 10000 
+    return std::rand() % 10000 + 1;
 }
 
 // Implementação dos métodos getter
+int Item::getId() const {
+    return id;
+}
+
 std::string Item::getNome() const {
     return nome;
 }
@@ -96,12 +80,50 @@ void Item::setQuantidade(int quantidade){
      this->quantidade = quantidade;
 }
 
-std::string Item::toJson() const {
-    nlohmann::json j;
+void Item::cadastrarItem(Inventario& inventario){
+    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja cadastrar:");
+    
+    // Verifica se já existe um item com o mesmo nome no inventário antes de adicionar
+    if(inventario.itemExiste(nome)){
+      // Se o Item existe------- +++++++++v
+      Interface::exibirMensagem("Erro: O Item ja esta cadastrado no sistema");
+    } else {
+      // Se o Item não existe, cria o item e adiciona ao inventário ------- +++++++++v
+      double valor = Interface::lerValor<double>("Digite o valor do item:");
 
-    j["nome"] = nome;
-    j["valor"] = valor;
-    j["quantidade"] = quantidade;
+      Item newItem = Item(nome, valor);
+      inventario.obterEstoque().insert(std::make_pair(newItem.getId(), newItem));
+      Interface::exibirMensagem("O item foi cadastrado no inventario!");
+    }
+}
 
-    return j.dump(); // Converte o JSON para uma string
+void Item::removerItem(Inventario& inventario){
+    std::string nome = Interface::lerValor<std::string>("Digite o nome do item que deseja remover");
+
+    // Verifica se já existe o item no inventário
+    if (inventario.itemExiste(nome)) {
+      // Se o Item existe, o remove do inventário ------- +++++++++v
+      Item& item = inventario.getItem(nome);
+      auto it = inventario.obterEstoque().find(item.getId());
+      inventario.obterEstoque().erase(it);
+      
+      Interface::exibirMensagem("O item foi Removido do inventario!");
+    } else {
+      Interface::exibirMensagem("Erro: O item não está cadastrado no inventário.");
+    }
+}
+
+void Item::atualizarValor(Inventario& inventario){
+    std::string nome = Interface::lerValor<std::string>("Digite o nome do item");
+
+    // Verifica se já existe o item no inventário
+    if (inventario.itemExiste(nome)) {
+      // Se o Item existe,  ------- +++++++++v
+       double valor = Interface::lerValor<int>("Digite o novo valor do item:");
+       inventario.getItem(nome).setValor(valor);
+       Interface::exibirMensagem("O valor do item foi autualizado");
+
+    } else {
+      Interface::exibirMensagem("O item não está cadastrado no inventário");
+    }
 }
--- Item.hpp
+++ Item.hpp
@@ -8,15 +8,24 @@ class Item {
 public:
     Item(const std::string& nome, double valor, int quantidade);
     Item(const std::string& nome, double valor);
+
     Item(const std::string& jsonString); 
-    
+
+    // Adicione um método à classe Item para serialização
+    std::string toJson() const;
+
     // Métodos getters e setters para os atributos do Item
+    /// @brief Retorna a quantidade de itens
+    ///
+    int getId() const;
+
     /// @brief Retorna o nome do item
     ///
     std::string getNome() const;
     /// @brief Retorna o valor do item
     ///
     double getValor() const;
+
     /// @brief Retorna a quantidade de itens
     ///
     int getQuantidade() const;
@@ -28,9 +37,8 @@ public:
     /// @brief 
     ///
     void setQuantidade(int quantidade);
-
-    // Adicione um método à classe Item para serialização
-    std::string toJson() const;
+    
+    static int gerarId();
 
     static void cadastrarItem(Inventario& inventario);
 
@@ -39,6 +47,7 @@ public:
     static void atualizarValor(Inventario& inventario);
 
 private:
+    int id;
     std::string nome;
     double valor;
     int quantidade;
