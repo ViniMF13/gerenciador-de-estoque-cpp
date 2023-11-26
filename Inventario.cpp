1460d0a84317e060cf687c860004e7e2cc8a6b6d
DAVISBH <davisdavi6@gmail.com>
2023-11-26 16:50:48 -0300

    quantidade metodo adicionarItens

--- Inventario.cpp
+++ Inventario.cpp
@@ -40,7 +40,12 @@ void Inventario::adicionarItens(){
   // Verifica se o item está no inventário -------= = = = = = 
   if (Inventario::itemExiste(nome)) {
     // se o item está no inventário -------= = = = = = 
-    int quantidade = Interface::lerValor<int>("Digite a quantidade de itens");    
+    int quantidade = -1;
+    while(quantidade < 0){
+        quantidade = Interface::lerValor<int>("Digite a quantidade de itens");    
+        if(quantidade < 0)
+            Interface::exibirMensagem("Valor invalido. ");
+    }
 
     int qtd = Inventario::getItem(nome).getQuantidade();
     
