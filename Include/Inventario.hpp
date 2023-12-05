58d7d03856a6b6624bd90c65d483f8d3f3287f6c
malu-lauar <147436427+malu-lauar@users.noreply.github.com>
2023-12-04 23:09:58 -0300

    doc Inventario.hpp

--- Include/Inventario.hpp
+++ Include/Inventario.hpp
@@ -10,34 +10,76 @@
 
 class Inventario {
 public:
-  /// @brief verifica se o item existe no inventário
-  ///
+  /**
+  * @brief Verifica se um item com o nome especificado existe.
+  *
+  * @param nome O nome do item a ser verificado.
+  * @return true se o item com o nome fornecido existir, false caso contrário.
+  */
   bool itemExiste(std::string& nome);
 
-  /// @brief retorna um item presente no inventario
-  ///
+  /**
+   * @brief Retorna uma referência para o item no inventário pelo nome.
+   *
+   * @param nome Nome do item a ser retornado.
+   * @return Referência para o item correspondente ao nome fornecido.
+   */
   Item& getItem(std::string& nome);
 
-  /// @brief Adiciona uma quantiade de itens ao estoque
-  ///
+  /**
+   * @brief Adiciona uma quantidade de itens ao estoque.
+   */
   void adicionarItens();
 
-  /// @brief Retira uma quantiade de itens do estoque
-  ///
+  /**
+   * @brief Retira uma quantidade de itens do estoque.
+   */
   void retirarItens();
 
+  /**
+   * @brief Adiciona uma movimentação ao histórico.
+   *
+   * @param nome Nome do item movimentado.
+   * @param tipo Tipo de movimentação (entrada ou saída).
+   * @param quantidade Quantidade movimentada.
+   */
   void adicionarMovimentacao(const std::string& nome, std::string tipo, int quantidade);
 
+  /**
+   * @brief Salva os dados do inventário em arquivos JSON.
+   *
+   * @param nomeArquivo1 Nome do arquivo para o estoque.
+   * @param nomeArquivo2 Nome do arquivo para o histórico.
+   */
   void salvarDados(const std::string& nomeArquivo1, const std::string& nomeArquivo2);
 
+  /**
+   * @brief Carrega os dados do inventário a partir de arquivos JSON.
+   *
+   * @param nomeArquivo1 Nome do arquivo para o estoque.
+   * @param nomeArquivo2 Nome do arquivo para o histórico.
+   */
   void carregarDados(const std::string& nomeArquivo1, const std::string& nomeArquivo2);
 
+  /**
+   * @brief Calcula o valor total dos itens no inventário.
+   * 
+   * @return Valor total do inventário.
+   */
   double calcularValorTotal() const;
   
-  /// @brief retorna o inventário
-  ///
+  /**
+   * @brief Retorna o estoque atual.
+   *
+   * @return Mapa contendo o estoque atual.
+   */
   std::map<int, Item>& obterEstoque();
 
+  /**
+   * @brief Retorna o histórico de movimentações do inventário.
+   * 
+   * @return Vetor contendo as movimentações.
+   */
   const std::vector<Movimentacao>& obterHistorico() const;
 
 
